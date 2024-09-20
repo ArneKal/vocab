c671f06a8b7f3d84e707b25a1dce73f0153548de
Arne Kaleck <assgatre@yahoo.de>
2024-09-20 12:37:47 +0200

    me dumb dumb

--- public/main.js
+++ public/main.js
@@ -24,7 +24,7 @@ function loadVocabulary() {
         return;
     }
 
-    fetch(`/vocabularies/${currentLanguage}.json`)
+    fetch(`/vocabularies/${currentLanguage}`)
         .then(response => {
             if (!response.ok) {
                 throw new Error(`Failed to load vocabulary for ${currentLanguage}`);
